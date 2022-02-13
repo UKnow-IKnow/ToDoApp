7fbb57c647bec0491a5a8dec8e2b9e052e5b4dba
UKnow-IKnow <sarojsahu014@gmail.com>
2022-02-13 18:05:53 +0530

    design Priority Drop down menu

--- app/src/main/java/com/example/todoapp/components/PriorityDropDown.kt
+++ app/src/main/java/com/example/todoapp/components/PriorityDropDown.kt
@@ -7,9 +7,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.ContentAlpha
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -18,6 +18,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 
@@ -42,12 +46,65 @@ fun PriorityDropDown(
         Canvas(
             modifier = Modifier
                 .size(16.dp)
-        ){
+                .weight(1f)
+        ) {
             drawCircle(color = priority.color)
         }
         Text(
+            modifier = Modifier.weight(8f),
             text = priority.name,
             style = MaterialTheme.typography.subtitle2
         )
+        IconButton(
+            modifier = Modifier
+                .alpha(ContentAlpha.medium)
+                .rotate(0f)
+                .weight(1.5f),
+            onClick = { expanded = true }
+        ) {
+            Icon(
+                imageVector = Icons.Filled.ArrowDropDown,
+                contentDescription = "Drop-Down Arrow Icon"
+            )
+        }
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onPrioritySelected(Priority.LOW)
+                }
+            ) {
+                PriorityItem(priority = Priority.LOW)
+            }
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onPrioritySelected(Priority.MEDIUM)
+                }
+            ) {
+                PriorityItem(priority = Priority.MEDIUM)
+            }
+            DropdownMenuItem(
+                onClick = {
+                    expanded = false
+                    onPrioritySelected(Priority.HIGH)
+                }
+            ) {
+                PriorityItem(priority = Priority.HIGH)
+            }
+        }
     }
+}
+
+
+@Composable
+@Preview
+fun PriorityDropDownPreview() {
+    PriorityDropDown(
+        priority = Priority.HIGH,
+        onPrioritySelected = {}
+    )
 }
