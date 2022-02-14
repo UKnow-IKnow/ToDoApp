d543ebba998291fb453d37fd744feb503f01079f
UKnow-IKnow <sarojsahu014@gmail.com>
2022-02-14 12:57:48 +0530

    starting with Task Content

--- app/src/main/java/com/example/todoapp/components/PriorityDropDown.kt
+++ app/src/main/java/com/example/todoapp/components/PriorityDropDown.kt
@@ -19,7 +19,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.tooling.preview.Preview
---
+++ app/src/main/java/com/example/todoapp/ui/theme/screens/list/task/TaskContent.kt
@@ -0,0 +1,42 @@
+package com.example.todoapp.ui.theme.screens.list.task
+
+import android.accounts.AuthenticatorDescription
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.todoapp.R
+import com.example.todoapp.data.models.Priority
+
+@Composable
+fun TaskContent(
+    title: String,
+    onTitleChange: (String) -> Unit,
+    description: String,
+    onDescriptionChange: (String) -> Unit,
+    priority: Priority,
+    onPrioritySelected: (Priority) -> Unit
+){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp)
+    ) {
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = title,
+            onValueChange = { onTitleChange(it) },
+            label = { Text(text = stringResource(id = R.string.title))},
+            textStyle = MaterialTheme.typography.body1,
+            singleLine = true
+        )
+
+    }
+}
