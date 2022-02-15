48715910f9ec3a6b20d94aa040c38314d1756233
UKnow-IKnow <sarojsahu014@gmail.com>
2022-02-15 17:19:44 +0530

    check validation in title or description

--- app/src/main/java/com/example/todoapp/ui/theme/screens/list/task/TaskScreen.kt
+++ app/src/main/java/com/example/todoapp/ui/theme/screens/list/task/TaskScreen.kt
@@ -1,8 +1,11 @@
 package com.example.todoapp.ui.theme.screens.list.task
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import com.example.todoapp.data.models.Priority
 import com.example.todoapp.data.models.ToDoTask
 import com.example.todoapp.ui.theme.viewModels.SharedViewModel
@@ -19,12 +22,22 @@ fun TaskScreen(
     val description: String by sharedViewModel.description
     val priority: Priority by sharedViewModel.priority
 
+    val context = LocalContext.current
+
     Scaffold(
         topBar = {
             TaskAppBar(
                 selectedTask = selectedTask,
                 navigateToListScreen = { action ->
-
+                    if(action == Action.NO_ACTION){
+                        navigateToListScreen(action)
+                    }else{
+                        if (sharedViewModel.validateField()) {
+                            navigateToListScreen(action)
+                        }else{
+                            displayToast(context = context)
+                        }
+                    }
                 }
             )
         },
@@ -46,4 +59,8 @@ fun TaskScreen(
         }
     )
 
+}
+
+fun displayToast(context: Context) {
+    Toast.makeText(context,"Field Empty.",Toast.LENGTH_SHORT).show()
 }
