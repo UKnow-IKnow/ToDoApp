23af7c8abf5d8e6a3c8cdbfc69a2090f496ead58
UKnow-IKnow <sarojsahu014@gmail.com>
2022-03-01 20:58:57 +0530

    adding animation in splash screen

--- app/src/main/java/com/example/todoapp/navigation/destination/SplashComposable.kt
+++ app/src/main/java/com/example/todoapp/navigation/destination/SplashComposable.kt
@@ -1,18 +1,26 @@
 package com.example.todoapp.navigation.destination
 
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideOutVertically
 import androidx.navigation.NavGraphBuilder
 import com.google.accompanist.navigation.animation.composable
 import com.example.todoapp.ui.theme.screens.list.splash.SplashScreen
 import com.example.todoapp.util.Constants.SPLASH
 
 
-@OptIn(ExperimentalAnimationApi::class)
+
+@ExperimentalAnimationApi
 fun NavGraphBuilder.splashComposable(
     navigateToListScreen: () -> Unit
 ){
     composable(
-        route = SPLASH
+        route = SPLASH,
+        exitTransition = {
+            slideOutVertically(
+                animationSpec = tween(800)
+            )
+        }
     ){
         SplashScreen(navigateToListScreen = navigateToListScreen)
     }
